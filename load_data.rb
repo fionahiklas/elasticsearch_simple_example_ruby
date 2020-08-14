ac46fe3a4a894c37e84e79887a9e942812d1ccfa
Fiona Bianchi <fiona@hiklas.co.uk>
2020-08-14 21:50:56 +0100

    Fixing dataset and more details in the script

--- load_data.rb
+++ load_data.rb
@@ -1,7 +1,59 @@
+#!/usr/bin/env ruby
+
 require 'optparse'
 require 'logger'
 require 'elasticsearch'
 require 'csv'
 
+log = Logger.new(STDOUT)
+
+headers = [
+  "cylinders",
+  "displ",
+  "drive",
+  "fuelType",
+  "fuelType1",
+  "make",
+  "model",
+  "mpgData",
+  "phevBlended",
+  "transmission",
+  "VClass",
+  "year"
+]
+
+options = {}
+OptionParser.new do |opts|
+
+  opts.banner = "Usage: load_data -f <file.csv> -e <Elastic search URL> [-d]"
+
+  opts.on("-f", "--file FILE", "Load CSV data from FILE") do |file|
+    options[:filename] = file
+  end
+
+  opts.on("-e", "--elasticsearch URL", "Connect to elastic search at URL") do |url|
+    options[:url] = url
+  end
+
+  opts.on("-d", "--dryrun", "Dry run") do 
+    options[:dry] = true
+  end
+end.parse!
+
+
+log.info("Options: #{options}")
+
+
+
+CSV.foreach(options[:filename], headers: true) do |line|
+  log.info("Line: #{line}")
+end
+
+
+  
+
+
+
+   
 
 
